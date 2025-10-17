c01965f7864054748fbb6966b7d128420d4dacf2
Mia Ware <miw828@lehigh.edu>
2025-10-17 00:29:00 -0400

    Mostly functional the win and turns work

--- script.js
+++ script.js
@@ -12,11 +12,11 @@
 
 
 let gameEnd = false; 
-const tiles = document.querySelectorAll("tiles"); // this gets ALL of the class that are named tiles in the HTML program 
+const tiles = document.querySelectorAll(".tiles"); // this gets ALL of the class that are named tiles in the HTML program 
 
 let playerSubmit = document.getElementById("submit"); // submit button
-let restart = document.getElementById("restart"); // restart button'
-
+let restart = document.getElementById("restart"); // restart button
+const playerHeader = document.getElementById("playerHeader"); 
 
 const playerOne = {
     player: document.getElementById("playerOne"), 
@@ -27,10 +27,15 @@ const playerTwo = {
     marker: 'O' 
 }
 
-let currentPlayer = playerOne ? playerTwo : playerOne; 
-const playerModal = document.getElementById("player"); 
+let currentPlayer = playerOne; // will switch later down the line
 
-let gameBoard = ["", "", "", "", "", "", "", "", ""]; // this is the gameBoad
+const playerModal = document.getElementById("player"); 
+const statusModal = document.getElementById("status"); 
+
+
+
+
+let gameBoard = ["", "", "", "", "", "", "", "", ""]; // this is the gameBoard
 
 const winConditions = [ 
       [0, 1, 2],
@@ -43,24 +48,65 @@ const winConditions = [
       [2, 4, 6],
     ];
 
-tiles.forEach((tiles, index) => { // creates the loop that 
 
-    tiles.addEventListener('click', () =>{
-        if(gameBoard[index] === ""){
-            gameBoard[index] = currentPlayer.marker; 
-            tiles.textContent = currentPlayer.marker; 
+tiles.forEach((tile, index) => { // this is an action with parameters 
+    // tile is the div thats being changed 
+    // index is the pointer in which the user is clicking 
+    tile.addEventListener('click', () => {// is tile is clicked
+        
+    
+        if (gameBoard[index] === "" && !gameEnd) { // if the tile is empty 
+            gameBoard[index] = currentPlayer.marker; // lets the system know gameBoard[0] = 'X
+            tile.textContent = currentPlayer.marker; // adds the textContent that it is equal to X
+           
+          
+        if(win()){
+        gameEnd = true; 
+    statusModal.style.display = "block"; 
+    statusModal.innerHTML +=`<h3>${currentPlayer.player.value}'s Wins!!</h3>`;
+    
+}
+
+
+currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne; // switched
+playerHeader.innerHTML = `<h3>${currentPlayer.player.value}'s Turn</h3>`; 
+             
+            
         }
+    });
+});
+
+
+function win(){
+   
+  for (let i = 0; i < winConditions.length; i++) { // creates a for loop 
+    const [a, b, c] = winConditions[i]; // groups each winCondition together 
+    
+    if (
+      gameBoard[a] !== "" &&  // if one tile is not empty and all three tiles are equal to one another there is a winner
+      gameBoard[a] === gameBoard[b] && 
+      gameBoard[a] === gameBoard[c]
+    ) {
+      return true; // there’s a winner
     }
-    )
-
-})
-
-
-playerSubmit.addEventListener("click", (e) => {  // this 
+  }
+  return false; // no win yet
+}
+playerSubmit.addEventListener("click", (e) => {  // this will just make the modal disappear
+    
     e.preventDefault(); 
     playerModal.style.display = "none"
-    document.getElementById("playerOne").value = ""; // resets the input
-    document.getElementById("playerTwo").value = ""; 
+     
 })
+
+
+
+    
+
+
+
+
+
+
 
 
